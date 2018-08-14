6d5f33efdbeed31615e0a9554934cc63e51d2e5b
khiemht <khiemht@KhiemHTs-MBP.net.fpt>
2018-08-15 00:23:34 +0700

    initialize

---
+++ HTKPinterest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTKPinterest
+//
+//  Created by khiemht on 8/15/18.
+//  Copyright © 2018 HTK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
